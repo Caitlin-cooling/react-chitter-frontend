73897cf4a0dcd0cd41528b027dbd1b6bdaa41db6
Caitlin Cooling <ccooling97@gmail.com>
2019-02-11 15:34:25 +0000

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import { Authentication } from './authentication'
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Chitter</h1>
+        <h1>Chitter</h1><br/>
+        <Authentication/>
       </div>
     );
   }
 }
-
-export default App;
