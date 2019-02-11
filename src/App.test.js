e344e41fb07047a939177dbb02466820a6866cad
Caitlin Cooling <ccooling97@gmail.com>
2019-02-11 14:53:56 +0000

    Refactor tests
    - Use describe block to separate tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('homepage', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('displays the title of the page', () => {
-  const div = shallow(<App/>)
-  const chitter = <h1>Chitter</h1>
-  expect(div.contains(chitter)).toEqual(true);
+  it('displays the title of the page', () => {
+    const div = shallow(<App/>)
+    const chitter = <h1>Chitter</h1>
+    expect(div.contains(chitter)).toEqual(true);
+  });
 });
