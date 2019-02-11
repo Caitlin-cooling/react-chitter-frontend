fb982a989c6b936775e8b0a315b8c7040fe3934a
Caitlin Cooling <ccooling97@gmail.com>
2019-02-11 15:42:33 +0000

    SignIn form is displayed on the page
    - Part of authentication
    - Tested

--- src/authentication.js
+++ src/authentication.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export class Authentication extends Component {
   render() {
     return(
-      <SignUp/>
+      <div>
+        <SignUp/>
+        <SignIn/>
+      </div>
     );
   }
 }
@@ -12,6 +15,17 @@ export var SignUp = function() {
   return <div id="form">
     <form>
       Sign Up
+      <input placeholer='Handle'></input>
+      <input placeholer='Password'></input>
+      <button>Submit</button>
+    </form>
+  </div>
+}
+
+export var SignIn = function() {
+  return <div id="form">
+    <form>
+      Sign In
       <input placeholer='Handle'></input>
       <input placeholer='Password'></input>
       <button>Submit</button>
--- src/authentication.test.js
+++ src/authentication.test.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Authentication, SignUp } from './authentication';
+import { Authentication, SignUp, SignIn } from './authentication';
 import { shallow } from 'enzyme';
 
 describe('Authentication', () => {
+  const form = shallow(<Authentication/>);
+
   it('renders a sign up form', () => {
-    const form = shallow(<Authentication/>);
     expect(form).toContainReact(<SignUp/>);
   });
 
+  it('renders a sign in form', () => {
+    expect(form).toContainReact(<SignIn/>);
+  });
+});
+
+describe('SignUp', () => {
   it('asks for handle and password with submit button', () => {
     const form = shallow(<SignUp/>);
+    expect(form).toContainReact(<input placeholer='Handle'></input>);
+    expect(form).toContainReact(<input placeholer='Password'></input>);
+    expect(form).toContainReact(<button>Submit</button>);
+  });
+});
+
+describe('SignIn', () => {
+  it('asks for handle and password with submit button', () => {
+    const form = shallow(<SignIn/>)
     expect(form).toContainReact(<input placeholer='Handle'></input>);
     expect(form).toContainReact(<input placeholer='Password'></input>);
     expect(form).toContainReact(<button>Submit</button>);
