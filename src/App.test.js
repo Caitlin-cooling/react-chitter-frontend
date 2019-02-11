cf87e4f0070ea2636a79168972f1407a47a7204c
Caitlin Cooling <ccooling97@gmail.com>
2019-02-11 14:40:13 +0000

    Chitter heading is displayed on the page
    - In the App component for now
    - Tested

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('displays the title of the page', () => {
+  const div = shallow(<App/>)
+  const chitter = <h1>Chitter</h1>
+  expect(div.contains(chitter)).toEqual(true);
 });
