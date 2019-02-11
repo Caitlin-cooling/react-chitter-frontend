3783d50852e9b4d16f86effa88e3ef304c3ca708
Caitlin Cooling <ccooling97@gmail.com>
2019-02-11 15:44:56 +0000

    Test to ensure that authentication is rendered in app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Authentication } from './authentication';
 import { shallow } from 'enzyme';
 
 describe('homepage', () => {
+  const div = shallow(<App/>)
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
@@ -11,8 +14,11 @@ describe('homepage', () => {
   });
 
   it('displays the title of the page', () => {
-    const div = shallow(<App/>)
     const chitter = <h1>Chitter</h1>
     expect(div.contains(chitter)).toEqual(true);
+  });
+
+  it('shows sign up and in options', () => {
+    expect(div).toContainReact(<Authentication/>)
   });
 });
